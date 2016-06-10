74cc9c2634f007bdc89df45615b86ef930c76695
liyueyang <liyueyang1991@163.com>
2016-06-10 22:20:41 +0800

    service worker

---
+++ h5_ServiceWorker/service-worker.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var cacheVersion = 0;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'offline.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+          './offline.svg',
+          offlineUrl
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+        })
+     );
+  }
+  else{
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
