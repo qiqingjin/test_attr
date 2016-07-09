13ec628253f932cc11c32bccc2a12e4b3cde3ccf
liyueyang <liyueyang1991@163.com>
2016-07-09 22:24:51 +0800

    co

---
+++ Nodejs_co/async.js
@@ -0,0 +1,55 @@
+fs = require('fs');
+co = require('co');
+
+var readFile = thunkify(fs.readFile);
+
+var gen = function* (){
+  var r1 = yield readFile('./hi.txt');
+  console.log(r1.toString());
+  var r2 = yield readFile('./hello.txt');
+  console.log(r2.toString());
+};
+
+console.log('------------------run thunk-----------------');
+run(gen);
+/*console.log('------------------run co--------------------');
+co(gen);*/
+
+function run(fn) {
+  var gen = fn();
+
+  function next(err, data) {
+    var result = gen.next(data);
+    console.log('--------next in thunk----------');
+    if (result.done) return;
+    result.value(next);
+  }
+
+  next();
+}
+function thunkify(fn){
+  return function(){
+    var args = new Array(arguments.length);
+    var ctx = this;
+
+    for(var i = 0; i < args.length; ++i) {
+      args[i] = arguments[i];
+    }
+
+    return function(done){
+      var called;
+
+      args.push(function(){
+        if (called) return;
+        called = true;
+        done.apply(null, arguments);
+      });
+
+      try {
+        fn.apply(ctx, args);
+      } catch (err) {
+        done(err);
+      }
+    };
+  };
+}
